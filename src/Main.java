dbf2932c40f6bae92c74c3a5da4a2c37d68d249d
fmmagalhaes <fran.mmagalhaes@gmail.com>
2017-07-10 17:21:39 +0100

    Main updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,31 +21,43 @@ public class Main {
 		String line = "";
 		while ((line = bis.readLine()) != null) {
 			line = line.trim();
-			String chordMatch = "[a-zA-Z](#|[a-zA-Z])*([0-9]*)?(M|m)?";
-			if (line.matches((chordMatch + "( +)?\\+( +)?[0-9]([0-9])?"))) {
-				//SUM
-				String[] parts = line.split("\\+");
-				String chord = parts[0];
-				int n = Integer.parseInt(parts[1]);
-				String newchord = calc.addSemitones(chord, n);
-				System.out.println(newchord + "\n");
-			} else if (line.matches((chordMatch + "( +)?-( +)?[0-9]([0-9])?"))) {
-				//SUBTRACT
-				String[] parts = line.split("-");
-				String chord = parts[0];
-				int n = Integer.parseInt(parts[1]);
-				String newchord = calc.subtractSemitones(chord, n);
-				System.out.println(newchord + "\n");
-			} else if (line.matches((chordMatch))) {
-				//COMPOSITION
-				String chord = line;
-				List<String> list = calc.getComposition(chord);
-				String composition = "";
-				for (String note : list)
-					composition += note + " ";
-				System.out.println("= " + composition + "\n");
-			} else {
-				System.out.println("usage: 'Chord =' or 'Chord + n'\n");
+			String chordMatch = "[CDEFGAB](#|b)?([a-zA-Z#]*[0-9]*)*";
+			try {
+				if (line.matches((chordMatch + "( *)\\+( *)[0-9]+"))) {
+					// SUM
+					String[] parts = line.split("\\+");
+					String chord = parts[0];
+					int n = Integer.parseInt(parts[1]);
+					String newchord = calc.addSemitones(chord, n);
+					System.out.println(newchord + "\n");
+				} else if (line.matches((chordMatch + "( *)-( *)?[0-9]+"))) {
+					// SUBTRACT
+					String[] parts = line.split("-");
+					String chord = parts[0];
+					int n = Integer.parseInt(parts[1]);
+					String newchord = calc.subtractSemitones(chord, n);
+					System.out.println(newchord + "\n");
+				} else if (line.matches("([CDEFGAB](#|b)?) ([CDEFGAB](#|b)? ?)+")) {
+					// CHORD FROM NOTES
+					List<String> notes = Arrays.asList(line.split(" "));
+					System.out.println(calc.getChordFromComposition(notes) + "\n");
+				} else { // if (line.matches((chordMatch))) {
+					/// COMPOSITION
+					String chord = line;
+					List<String> list = null;
+					list = calc.getComposition(chord);
+
+					// for debugging
+					System.out.println("Root = " + calc.getRootNote(chord));
+					System.out.println("Symbols = " + calc.getSymbols(chord, true));
+
+					String composition = "";
+					for (String note : list)
+						composition += note + " ";
+					System.out.println("= " + composition + "\n");
+				}
+			} catch (UnknownNoteException e) {
+				System.out.println(e.getMessage() + "\n");
 			}
 		}
 	}
